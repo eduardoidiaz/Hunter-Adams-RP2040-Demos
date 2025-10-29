6522a5e9d6ca293191b458b70d9727b8b81f2ab3
Jack Chaney <jbc282@cornell.edu>
2025-10-29 16:25:15 -0400

    lab3w3 checkoff

--- Sensors/MPU6050_IMU_Demo/imu_demo.c
+++ Sensors/MPU6050_IMU_Demo/imu_demo.c
@@ -61,27 +61,30 @@ fix15 acceleration[3], gyro[3] ;
 // Complementary Filter vars
 volatile float comp_angle_roll = 0.0 ; // filtered angle degrees
 const float dt = 0.001 ; // time step in seconds (1.0/1000 Hz)
-const float percent_gyro = 0.90 ; // filter coefficient (how much of gyro to mix in)
+const float percent_gyro = 0.97 ; // filter coefficient (how much of gyro to mix in)
 
 // P-controller vars
-volatile float targ_ang = 0.0 ;
-volatile float Kp = 42.0 ;
-const int pwm_max = WRAPVAL ;
+volatile float targ_ang = -100.0 ;
+volatile float Kp = 350.0 ; // proportial gain
+const int pwm_max = 4000 ;
 const int pwm_min = 0 ;
-const int pwm_neut = 2500;
+const int pwm_neut = 0; // idle value
 
 // other controller vars
-volatile float Ki = 0.0 ;
+volatile float Ki = 35 ; // integral gain
 volatile float integral_term = 0.0 ;
-volatile float Kd = 2.0 ;
+volatile float Kd = 120 ; // derivative gain
 volatile float d_term = 0.0 ;
 static int d_term_count = 0 ;
+static float d_filter = 0.3 ; // amount of old term will be mixed with new term (low passing deriv) anything above 50KHz(?) is filtered?
+volatile float d_term_prev = 0.0 ;
 volatile float prev_angle = 0 ;
 const float integral_deadband = 25.0 ;
 
 // lowpass control signal vars
 volatile float filt_control = pwm_neut ;
-const float control_filt_coeff = 0.3 ;
+volatile float filt_video = pwm_neut ;
+const float control_filt_coeff = 1.0 ;
 
 // character array
 char screentext[40];
@@ -97,9 +100,31 @@ int threshold = 10 ;
 // semaphore
 static struct pt_sem vga_semaphore ;
 
+// for displaying parameters on the VGA
+static struct pt_sem label_semaphore ;
+char target_angle_buffer[10] ;
+char prop_buffer[10] ;
+char int_buffer[10] ;
+char div_buffer[10] ;
+
 // PWM duty cycle vars
 volatile int control ;
 volatile int old_control ;
+
+// for running angle program
+#define PIN_BUTTON 15 // gpio 15 (pin 20), use ground pin 18
+
+static struct pt_sem sequence_semaphore ;
+
+// state machine variables - debouncing
+// states
+#define NOT_PRESSED 0
+#define MAYBE_PRESSED 1
+#define PRESSED 2
+#define MAYBE_NOT_PRESSED 3
+volatile int possible = 1 ; // init high
+volatile unsigned int STATE_0 = NOT_PRESSED ;
+
 
 // PWM Interrupt service routine
 void on_pwm_wrap() {
@@ -122,31 +147,26 @@ void on_pwm_wrap() {
     // prev_angle + (rate * dt)
     float gyro_angle_roll = comp_angle_roll + (gyro_x_rate * dt) ;
 
-    // do complementary filtering
-    comp_angle_roll = percent_gyro * gyro_angle_roll 
-    + (1.0 - percent_gyro) * accel_angle_roll ;
+    // do complementary filtering (referenced in instructions)
+    comp_angle_roll = (percent_gyro * gyro_angle_roll) + ((1.0 - percent_gyro) * accel_angle_roll) ;
     /////////////// COMP FILTER END ////////////////////////
 
     ///////////////   PID CONTROLLER  ////////////////////////
     float error = targ_ang - comp_angle_roll ;
     
-    // prop term calc
+    // proportinal term calc
     float p_term = Kp * error ;
 
-    // int term calc
+    // integral term calc
     float i_term = Ki * integral_term ;
 
-    // deriv term calc
-    // float derivative = (comp_angle_roll - prev_angle) / dt ;
-    // float d_term = Kd * derivative ;
-    // prev_angle = comp_angle_roll ;
-    d_term_count++;
-    if (d_term_count > 2) {
-        float derivative = (comp_angle_roll - prev_angle) / (dt * 3.0) ;
-        d_term = Kd * derivative ;
-        prev_angle = comp_angle_roll ;
-        d_term_count = 0 ;
-    }
+    // Deriv term calc
+    d_term = -Kd * gyro_x_rate ;
+    //prev_angle = comp_angle_roll ;
+
+    // LOW PASS FILTER
+    d_term = (d_filter * (float) d_term) + ((1.0 - d_filter) * d_term_prev) ;
+    d_term_prev = d_term ;
 
     // Calc control signal 
     int new_control =  pwm_neut + (int)(p_term + i_term - d_term);
@@ -173,6 +193,7 @@ void on_pwm_wrap() {
 
     // LOW PASS FILTER
     filt_control = (control_filt_coeff * (float) new_control) + ((1.0 - control_filt_coeff) * filt_control) ;
+    filt_video = (0.3 * (float) new_control) + ((0.7) * filt_video) ;
 
     // Update volatile control var to enact pwm changes calculated
     control = (int)filt_control ;
@@ -187,6 +208,7 @@ void on_pwm_wrap() {
         pwm_set_chan_level(slice_num, PWM_CHAN_A, control) ;
     }
 }
+
 
 // Thread that draws to VGA display
 static PT_THREAD (protothread_vga(struct pt *pt))
@@ -247,7 +269,7 @@ static PT_THREAD (protothread_vga(struct pt *pt))
     sprintf(screentext, "-90") ;
     setCursor(45, 225) ;
     writeString(screentext) ;
-    
+
 
     while (true) {
         // Wait on semaphore
@@ -272,7 +294,7 @@ static PT_THREAD (protothread_vga(struct pt *pt))
             drawVLine(xcoord, 0, 480, BLACK) ;
 
             // Draw bottom plot (motor sig)
-            int motor_y = motor_y_bot - (int)(motor_px_h * ((float)filt_control - motor_min) / motor_rng) ;
+            int motor_y = motor_y_bot - (int)(motor_px_h * ((float)filt_video - motor_min) / motor_rng) ;
             // plot bounding
             if (motor_y < motor_y_top) motor_y = motor_y_top ;
             if (motor_y > motor_y_bot) motor_y = motor_y_bot ;
@@ -291,7 +313,22 @@ static PT_THREAD (protothread_vga(struct pt *pt))
             else {
                 xcoord = 81 ;
             }
+            // Draw labels
+            fillRect(450, 50, 200, 40, BLACK) ;
+            setCursor(450, 50) ;
+            writeString("Target Angle: ") ;
+            writeString(target_angle_buffer) ;
+            setCursor(450, 60) ;
+            writeString("Kp: ") ;
+            writeString(prop_buffer) ;
+            setCursor(450, 70) ;
+            writeString("Ki: ") ;
+            writeString(int_buffer) ;
+            setCursor(450, 80) ;
+            writeString("Kd: ") ;
+            writeString(div_buffer) ;
         }
+        PT_YIELD(pt);
     }
     // Indicate end of thread
     PT_END(pt);
@@ -328,9 +365,12 @@ static PT_THREAD (protothread_serial(struct pt *pt))
             serial_read ;
             sscanf(pt_serial_in_buffer, "%f", &float_in) ;
             targ_ang = float_in ;
-            integral_term = 0.0 ; // reset integral term for new target
+            // integral_term = 0.0 ; // reset integral term for new target
             sprintf(pt_serial_out_buffer, "Target angle set to %.2f\n", targ_ang) ;
             serial_write ;
+            // for printed output
+            snprintf(target_angle_buffer, 10, "%.1f", targ_ang) ;
+            
         }
         else if (classifier =='p') {
             sprintf(pt_serial_out_buffer, "Proportional Gain (float): ");
@@ -341,6 +381,9 @@ static PT_THREAD (protothread_serial(struct pt *pt))
             Kp = float_in ;
             sprintf(pt_serial_out_buffer, "Kp set to: %.2f\n", Kp) ;
             serial_write ;
+            // for printed output
+            snprintf(prop_buffer, 10, "%.0f", Kp) ;
+            
         }
         else if (classifier == 'i') {
             sprintf(pt_serial_out_buffer, "input a integral gain (Ki): ");
@@ -351,6 +394,8 @@ static PT_THREAD (protothread_serial(struct pt *pt))
             Ki = float_in ;
             sprintf(pt_serial_out_buffer, "Ki set to %.2f\n", Ki) ;
             serial_write ;
+            snprintf(int_buffer, 10, "%.0f", Ki) ;
+            
         }
         else if (classifier == 'd') {
             sprintf(pt_serial_out_buffer, "input a derivative gain (Kd): ");
@@ -361,14 +406,116 @@ static PT_THREAD (protothread_serial(struct pt *pt))
             Kd = float_in ;
             sprintf(pt_serial_out_buffer, "Kd set to %.2f\n", Kd) ;
             serial_write ;
+            // for printed output
+            snprintf(div_buffer, 10, "%.0f", Kd) ;
+            
         }
+        PT_YIELD(pt);
     }
     PT_END(pt) ;
 }
 
+static PT_THREAD(protothread_debouncing(struct pt *pt)) 
+{
+  PT_BEGIN(pt);
+
+  while(1) {
+    int i = gpio_get(PIN_BUTTON) ; // value of press
+    
+    // implementing this debouncing algorithm with switch for clarity rather than if statements
+    switch (STATE_0) {
+      case NOT_PRESSED :
+        if (i == 0) { // if the button is low (pressed)
+          STATE_0 = MAYBE_PRESSED ;
+          possible = i ; 
+        }
+        break ;
+      case MAYBE_PRESSED :
+        if (i == possible) {
+          STATE_0 = PRESSED ;
+          PT_SEM_SIGNAL(pt, &sequence_semaphore) ;
+          printf("button") ;
+        }
+        else {
+          STATE_0 = NOT_PRESSED ;
+        }
+        break ;
+      case PRESSED :
+        if (i == 1) { // if the button is seen high again, maybe not pressed
+          STATE_0 = MAYBE_NOT_PRESSED ;
+          possible = i ;
+        }
+        break ;
+      case MAYBE_NOT_PRESSED :
+        if (i == possible) { //  possible is 1 right now, so if it is high send to not pressed
+          STATE_0 = NOT_PRESSED ;
+        }
+        else {
+          STATE_0 = PRESSED ;
+        }
+        break ;
+    }
+    
+    // yield for necessary amount of time
+    PT_YIELD(pt) ;
+  }
+  PT_END(pt) ;
+} // thread for the debouncing
+
+
+static PT_THREAD(protothread_sequence(struct pt *pt)) 
+{
+  PT_BEGIN(pt);
+
+  while(1) {
+    PT_SEM_WAIT(pt, &sequence_semaphore) ;
+    printf("meow") ;
+
+    // t < 0, beam is vertically down
+    // when button released:
+    // t = 0 target angle horizontal
+    targ_ang = -199 ;
+    // integral_term = 0.0 ; // reset integral term for new target
+    // for printed output
+    snprintf(target_angle_buffer, 10, "%.1f", targ_ang) ;
+    PT_YIELD_usec(5000000) ; // yield 5 seconds
+
+    targ_ang = 0;
+    snprintf(target_angle_buffer, 10, "%.1f", targ_ang) ;
+    PT_YIELD_usec(5000000) ; // yield 5 seconds
+
+    // t = 5 target angle ~30 degrees above horizontal
+    targ_ang = 30 ;
+    integral_term = 0.0 ; // reset integral term for new target
+    // for printed output
+    snprintf(target_angle_buffer, 10, "%.1f", targ_ang) ;
+    PT_YIELD_usec(5000000) ; // yield 5 seconds
+
+    // t = 10 target angle ~30 degrees below horizontal
+    targ_ang = -30 ;
+    integral_term = 0.0 ; // reset integral term for new target
+    // for printed output
+    snprintf(target_angle_buffer, 10, "%.1f", targ_ang) ;
+    PT_YIELD_usec(5000000) ; // yield 5 seconds
+
+    // t = 15 target angle horizontal
+    targ_ang = 0 ;
+    integral_term = 0.0 ; // reset integral term for new target
+    // for printed output
+    snprintf(target_angle_buffer, 10, "%.1f", targ_ang) ;
+    
+    
+    PT_YIELD(pt) ;
+  }
+  PT_END(pt) ;
+} // thread for the sequence
+
+
 // Entry point for core 1
 void core1_entry() {
     pt_add_thread(protothread_vga) ;
+    pt_add_thread(protothread_sequence) ;
+    pt_add_thread(protothread_debouncing) ;
     pt_schedule_start ;
 }
 
@@ -382,6 +529,37 @@ int main() {
 
     // Initialize VGA
     initVGA() ;
+    setTextSize(1) ;
+    setTextColor(WHITE);
+
+    // put titles for the graph and initial gains/inputs
+    setCursor(10, 50) ;
+    writeString("Angle of Arm") ;
+    setCursor(10, 255) ;
+    writeString("Motor Signal") ;
+
+    snprintf(target_angle_buffer, 10, "%.1f", targ_ang) ;
+    snprintf(prop_buffer, 10, "%.0f", Kp) ;
+    snprintf(int_buffer, 10, "%.0f", Ki) ;
+    snprintf(div_buffer, 10, "%.0f", Kd) ;
+
+    setCursor(450, 50) ;
+    writeString("Target Angle: ") ;
+    writeString(target_angle_buffer) ;
+    setCursor(450, 60) ;
+    writeString("Kp: ") ;
+    writeString(prop_buffer) ;
+    setCursor(450, 70) ;
+    writeString("Ki: ") ;
+    writeString(int_buffer) ;
+    setCursor(450, 80) ;
+    writeString("Kd: ") ;
+    writeString(div_buffer) ;
+
+    // initialize GPIO for the button 
+    gpio_init(PIN_BUTTON) ;
+    gpio_set_dir(PIN_BUTTON, GPIO_IN); // set GPIO to input
+    gpio_pull_up(PIN_BUTTON) ; // drive the pin normally high, if button pressed will be low
 
     ////////////////////////////////////////////////////////////////////////
     ///////////////////////// I2C CONFIGURATION ////////////////////////////
--- Sensors/MPU6050_IMU_Demo/mpu6050.c
+++ Sensors/MPU6050_IMU_Demo/mpu6050.c
@@ -8,41 +8,44 @@
 #include "mpu6050.h"
 #include "pico/time.h"
 #include <pico/types.h>
-#define TIMEOUT 900 // us for timeout
+#define TIMEOUT 5000 // us for timeout
+
+absolute_time_t maketimeout() {
+    return make_timeout_time_us(TIMEOUT);
+}
 
 void mpu6050_reset() {
     // Two byte reset. First byte register, second byte data
     // There are a load more options to set up the device in
     // different ways that could be added here
     uint8_t buf[] = {0x6B, 0x00};
-    absolute_time_t timeout;
-    timeout = make_timeout_time_us(TIMEOUT) ;
-    i2c_write_blocking_until(I2C_CHAN, ADDRESS, buf, 2, false, timeout);
+    // timeout = make_timeout_time_us(TIMEOUT) ;
+    i2c_write_blocking_until(I2C_CHAN, ADDRESS, buf, 2, false, maketimeout());
 
     // Set gyro sample rate (set to 1KHz, same as accel)
     uint8_t gyro_rate[] = {0x19, 0b00000111} ;
-    timeout = make_timeout_time_us(TIMEOUT) ;
-    i2c_write_blocking_until(I2C_CHAN, ADDRESS, gyro_rate, 2, false, timeout);
+    // timeout = make_timeout_time_us(TIMEOUT) ;
+    i2c_write_blocking_until(I2C_CHAN, ADDRESS, gyro_rate, 2, false, maketimeout());
 
     // Configure the Gyro range (+/- 250 deg/s)
     uint8_t gyro_settings[] = {0x1b, 0b00000000} ;
-    timeout = make_timeout_time_us(TIMEOUT) ;
-    i2c_write_blocking_until(I2C_CHAN, ADDRESS, gyro_settings, 2, false, timeout);
+    // timeout = make_timeout_time_us(TIMEOUT) ;
+    i2c_write_blocking_until(I2C_CHAN, ADDRESS, gyro_settings, 2, false, maketimeout());
 
     // Configure the Accel range (+/- 2g's)
     uint8_t accel_settings[] = {0x1c, 0b00000000} ;
-    timeout = make_timeout_time_us(TIMEOUT) ;
-    i2c_write_blocking_until(I2C_CHAN, ADDRESS, accel_settings, 2, false, timeout);
+    // timeout = make_timeout_time_us(TIMEOUT) ;
+    i2c_write_blocking_until(I2C_CHAN, ADDRESS, accel_settings, 2, false, maketimeout());
 
     // Configure interrupt pin
     uint8_t pin_settings[] = {0x37, 0b00010000} ;
-    timeout = make_timeout_time_us(TIMEOUT) ;
-    i2c_write_blocking_until(I2C_CHAN, ADDRESS, pin_settings, 2, false, timeout);
+    // timeout = make_timeout_time_us(TIMEOUT) ;
+    i2c_write_blocking_until(I2C_CHAN, ADDRESS, pin_settings, 2, false, maketimeout());
 
     // Configure data ready interrupt
     uint8_t int_config[] = {0x38, 0x01} ;
-    timeout = make_timeout_time_us(TIMEOUT) ;
-    i2c_write_blocking_until(I2C_CHAN, ADDRESS, int_config, 2, false, timeout);
+    // timeout = make_timeout_time_us(TIMEOUT) ;
+    i2c_write_blocking_until(I2C_CHAN, ADDRESS, int_config, 2, false, maketimeout());
 }
 
 void mpu6050_read_raw(fix15 accel[3], fix15 gyro[3]) {
@@ -52,14 +55,13 @@ void mpu6050_read_raw(fix15 accel[3], fix15 gyro[3]) {
 
     uint8_t buffer[6];
     int16_t temp_accel, temp_gyro ;
-    absolute_time_t timeout;
 
     // Start reading acceleration registers from register 0x3B for 6 bytes
     uint8_t val = 0x3B;
-    timeout = make_timeout_time_us(TIMEOUT) ;
-    i2c_write_blocking_until(I2C_CHAN, ADDRESS, &val, 1, true, timeout); // true to keep master control of bus
-    timeout = make_timeout_time_us(TIMEOUT);
-    i2c_read_blocking_until(I2C_CHAN, ADDRESS, buffer, 6, false, timeout);
+    // timeout = make_timeout_time_us(TIMEOUT) ;
+    i2c_write_blocking_until(I2C_CHAN, ADDRESS, &val, 1, true, maketimeout()); // true to keep master control of bus
+    // timeout = make_timeout_time_us(TIMEOUT);
+    i2c_read_blocking_until(I2C_CHAN, ADDRESS, buffer, 6, false, maketimeout());
 
     for (int i = 0; i < 3; i++) {
         temp_accel = (buffer[i<<1] << 8 | buffer[(i<<1) + 1]);
@@ -70,10 +72,10 @@ void mpu6050_read_raw(fix15 accel[3], fix15 gyro[3]) {
     // Now gyro data from reg 0x43 for 6 bytes
     // The register is auto incrementing on each read
     val = 0x43;
-    timeout = make_timeout_time_us(TIMEOUT) ;
-    i2c_write_blocking_until(I2C_CHAN, ADDRESS, &val, 1, true, timeout);
-    timeout = make_timeout_time_us(TIMEOUT);
-    i2c_read_blocking_until(I2C_CHAN, ADDRESS, buffer, 6, false, timeout);  // False - finished with bus
+    // timeout = make_timeout_time_us(TIMEOUT) ;
+    i2c_write_blocking_until(I2C_CHAN, ADDRESS, &val, 1, true, maketimeout());
+    // timeout = make_timeout_time_us(TIMEOUT);
+    i2c_read_blocking_until(I2C_CHAN, ADDRESS, buffer, 6, false, maketimeout());  // False - finished with bus
 
     for (int i = 0; i < 3; i++) {
         temp_gyro = (buffer[i<<1] << 8 | buffer[(i<<1) + 1]);
